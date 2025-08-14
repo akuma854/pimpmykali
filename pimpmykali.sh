bf0e70b57fd517b180f71685288a272555b302d8
Bornunique911 <69379200+Bornunique911@users.noreply.github.com>
2025-08-14 18:12:28 +0530

    Update pimpmykali.sh
    Fixed VSCode installation steps according to the official documentation.

--- pimpmykali.sh
+++ pimpmykali.sh
@@ -1833,10 +1833,9 @@ install_vscode() {
 
     is_installed "wget gpg apt_transport_https"
 
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
-    sudo install -D -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-    rm -f /tmp/packages.microsoft.gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+    rm -f microsoft.gpg
     #required-apt-update
     apt_update
     is_installed code
